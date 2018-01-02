25499d341d42cd4a5acb8e4a8b40ce29667f4c93
jakeogh <github.com@v6y.net>
2018-01-01 22:02:48 -0700

    auto-commit

--- youtube_dl_wrapper/youtube_dl_wrapper.py
+++ youtube_dl_wrapper/youtube_dl_wrapper.py
@@ -149,7 +149,8 @@ def check_if_video_exists_by_video_id(video_id):
         matches.append(match)
     if matches:
         eprint("matches:", matches)
-        return matches
+        assert len(matches) == 1
+        return matches[0]
     raise NoMatchException
 
 
