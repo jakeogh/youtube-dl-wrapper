a0ab5ba44806008f652f8f2ab85abbc038f0fbb9
jakeogh <github.com@v6y.net>
2018-01-02 01:22:09 -0700

    auto-commit

--- youtube_dl_wrapper/youtube_dl_wrapper.py
+++ youtube_dl_wrapper/youtube_dl_wrapper.py
@@ -171,7 +171,7 @@ def download_url(url, cache_dir):
         'allsubtitles': True,
         'progress_with_newline': False,
         'logger': MyLogger(),
-        'exec_cmd': 'ls {}',
+        'exec_cmd': 'ls -al {} >> /home/user/temp_log_delme',
     }
 
 
