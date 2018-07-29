ec9d94bdbb2adfa742b58cb210519f9ee25201c3
jakeogh <github.com@v6y.net>
2018-07-29 13:15:29 -0700

    remove duplicate filename fields

--- youtube_dl_wrapper/youtube_dl_wrapper.py
+++ youtube_dl_wrapper/youtube_dl_wrapper.py
@@ -152,7 +152,7 @@ def download_url(url, cache_dir, play):
         'verbose': False,
         'forcefilename': True,
         'socket_timeout': 30,
-        'outtmpl': cache_dir + '/sources/' + '%(extractor)s' + '/' + '%(uploader)s' + '/' + "%(uploader)s__%(uploader_id)s__%(upload_date)s__%(title)s__%(extractor)s__%(id)s.%(ext)s",
+        'outtmpl': cache_dir + '/sources/' + '%(extractor)s' + '/' + '%(uploader)s' + '/' + "%(uploader_id)s__%(upload_date)s__%(title)s__%(id)s.%(ext)s",
         'ignoreerrors': True,
         'continue': True,
         'retries': 20,
