428e0313c4f48ae509b39653af2f8ee808a7679d
jakeogh <github.com@v6y.net>
2019-03-14 21:31:23 -0700

    auto-commit

--- youtube_dl_wrapper/youtube_dl_wrapper.py
+++ youtube_dl_wrapper/youtube_dl_wrapper.py
@@ -103,7 +103,7 @@ def get_filename_for_url(url):
         'skip_download': True,
     }
     with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.download(url, download=False, process=False)
+        info = ydl.download(url)
         print(info)
         #try:
         #    if info['id']:
