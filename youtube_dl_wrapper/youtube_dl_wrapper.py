fc774f168eaf09eb02bf58717e37b6deb54f8558
jakeogh <github.com@v6y.net>
2020-03-20 11:26:52 -0700

    auto-commit

--- youtube_dl_wrapper/youtube_dl_wrapper.py
+++ youtube_dl_wrapper/youtube_dl_wrapper.py
@@ -249,13 +249,14 @@ def look_for_output_file_variations(output_file):
 @click.option('--ignore-download-archive', is_flag=True)
 @click.option('--play', is_flag=True)
 @click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
 @click.option('--destdir', is_flag=False, required=False, default='~/_youtube')
 @click.option('--archive-file', is_flag=False, required=False, default='~/.youtube_dl.cache')
-def youtube_dl_wrapper(urls, id_from_url, ignore_download_archive, play, verbose, destdir, archive_file):
+def youtube_dl_wrapper(urls, id_from_url, ignore_download_archive, play, verbose, debug, destdir, archive_file):
     ic(verbose)
     if not urls:
         ceprint("no args, checking clipboard for urls")
-        urls = get_clipboard_iris(verbose=verbose)
+        urls = get_clipboard_iris(verbose=debug)
         if verbose:
             ic(urls)
 
